a9a0443e8e5f539479ba29260732d5179afac9a3
NikolG <greplova.nikol@gmail.com>
2026-03-17 12:59:31 +0100

    Data progress

--- data/cleaning.py
+++ data/cleaning.py
@@ -1,8 +1,13 @@
 import csv
 
-# Load in:
+# File which is only cleaning up
+
+# Load in from raw:
 
 # TODO: Strip white spacing 
 # TODO: Split author and book into two separate columns
-# TODO: Convert date into proper formatting 
+# TODO: Remove time from date
+# TODO: Convert date into proper formatting and add year 2025
+# TODO: Maybe change the sorting to be in chronological order
 
+# Write into a new folder
