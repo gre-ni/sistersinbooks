2ccf0eca502789a9789e30131967bc7b227dd1c4
NikolG <greplova.nikol@gmail.com>
2026-03-18 11:57:16 +0100

    data cleaning script

--- data/cleaning.py
+++ data/cleaning.py
@@ -1,13 +1,35 @@
 import csv
+import sys
+import re
 
-# File which is only cleaning up
+def main():
+    
+    if len(sys.argv) != 2:
+        sys.exit("Provide file to clean as command line argument.")
 
-# Load in from raw:
+    try: 
+        with open(f"raw/{sys.argv[1]}") as file:
+            reader = csv.DictReader(file)
+            books = [dict(row) for row in reader]         
+        
+    except FileNotFoundError:
+        sys.exit("File not found.")
+    
+    year = f"{sys.argv[1]}".strip(".csv")
+    
+    for book in books:
+        book["type"] = book["type"].strip().lower()
+        title_author = re.split(r" by |, | - ", book["book"])
+        book["title"] = title_author[0].strip()
+        book["author"] = title_author[1].strip()
+        date_parts = re.split(r"\.| - ", book["date"])
+        book["date"] = f"{year}-{date_parts[1]}-{date_parts[0]}"
+        del book["book"]
+    
+    with open(f"processed/{sys.argv[1]}", "w") as file:
+        writer = csv.DictWriter(file, fieldnames=["title", "author", "type", "date", "rating"])
+        for book in books:
+            writer.writerow({"title": book["title"], "author": book["author"], "type": book["type"], "date": book["date"], "rating": book["rating"]})
 
-# TODO: Strip white spacing 
-# TODO: Split author and book into two separate columns
-# TODO: Remove time from date
-# TODO: Convert date into proper formatting and add year 2025
-# TODO: Maybe change the sorting to be in chronological order
-
-# Write into a new folder
+if __name__ == "__main__":
+    main()
