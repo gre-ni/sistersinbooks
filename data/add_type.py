ad65035e95c97d9dcb0de35743c649d4aa55f060
NikolG <greplova.nikol@gmail.com>
2026-04-29 22:23:42 +0200

    Adding book types to database

--- data/add_type.py
+++ data/add_type.py
@@ -1,7 +1,10 @@
 import csv
 import sqlite3
 
-with sqlite3.connect("books.db") as con, open("books.csv") as f:
+with sqlite3.connect("books.db") as con, open("all-books.csv") as f:
     con.row_factory = sqlite3.Row
     db = con.cursor()
     reader = csv.DictReader(f)
+    
+    for row in reader:
+        db.execute("UPDATE books SET type = ? WHERE title = ?", (row["type"], row["title"]))
