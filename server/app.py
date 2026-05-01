21edf7a78b6b07e9c9361b15192ea37e89953d2c
NikolG <greplova.nikol@gmail.com>
2026-05-01 13:52:16 +0200

    endpoint for individual years + slug fix

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, request, jsonify
 from flask_cors import CORS
 from pathlib import Path
 import sqlite3
+import utils
 
 
 app = Flask(__name__)
@@ -19,6 +20,22 @@ def books():
         db = con.cursor()
         result = db.execute("SELECT * FROM books").fetchall()
         return jsonify([dict(row) for row in result]), 201
+
+# TODO: Endpoint which returns only books with one year
+@app.route("/books/year", methods=["GET"])
+def books_yearly():
+    with sqlite3.connect(DB_PATH) as con:
+        
+        # Check that it is a valid year:
+        year = request.args.get("year")
+        if year not in utils.YEARS:
+            return jsonify([{"error": "Invalid Year"}]), 404
+        
+        
+        con.row_factory = sqlite3.Row
+        db = con.cursor()
+        result = db.execute("SELECT * FROM books WHERE date LIKE ?", (f"{year}%",)).fetchall()
+        return jsonify([dict(row) for row in result]), 200
 
 
 # TODO: Endpoint which returns only two upcoming books, one classic and one modern:
