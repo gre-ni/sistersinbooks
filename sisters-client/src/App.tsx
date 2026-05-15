3a469f149f572439488d74d6331d05078ab80b9d
NikolG <greplova.nikol@gmail.com>
2026-05-15 15:21:24 +0200

    clean up index

--- sisters-client/src/App.tsx
+++ sisters-client/src/App.tsx
@@ -1,14 +1,9 @@
-import { YEARS } from "./utils.ts";
-import { BookList } from "./components/sections/BookList.tsx";
 import { Overview } from "./components/sections/Overview.tsx";
 
 function App() {
     return (
         <div>
             <Overview />
-            {YEARS.map((year) => (
-                <BookList year={year} key={year} />
-            ))}
         </div>
     );
 }
